3fc04cdd0d5a16ff77039b9d25e71ae1e6480c91
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-20 21:41:06 -0500

    ErrorBoundary added

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,33 +1,36 @@
 import { Link, NavLink } from "react-router-dom";
 import React from "react";
 import LinkCH from "../components/LinkCH/LinkCH";
+import ErrorBoundary from "../ErrorBoundry/ErrorBoundary";
 
 export default function HomePage() {
     return (
         <React.Fragment>
             <h1>Home Page</h1>
-            <nav
-                style={ {
-                    borderBottom: "solid 1px",
-                    paddingBottom: "1rem"
-                } }
-            >
-                <Link to="/users">Users</Link> | { " " }
-                <Link to="/items">Items</Link> | { " " }
-                <LinkCH page={ "/users" }>Go to Users page</LinkCH> | { " " }
-                <NavLink
-                    style={ ( { isActive } ) => {
-                        return {
-                            display: "block",
-                            margin: "1rem 0",
-                            color: isActive ? "green" : "blue"
-                        };
+            <ErrorBoundary>
+                <nav
+                    style={ {
+                        borderBottom: "solid 1px",
+                        paddingBottom: "1rem"
                     } }
-                    to={ '/users' }
                 >
-                    Nav Link
-                </NavLink>
-            </nav>
+                    <Link to="/users">Users</Link> | { " " }
+                    <Link to="/items">Items</Link> | { " " }
+                    <LinkCH page={ "/users" }>Go to Users page</LinkCH> | { " " }
+                    <NavLink
+                        style={ ( { isActive } ) => {
+                            return {
+                                display: "block",
+                                margin: "1rem 0",
+                                color: isActive ? "green" : "blue"
+                            };
+                        } }
+                        to={ '/users' }
+                    >
+                        Nav Link
+                    </NavLink>
+                </nav>
+            </ErrorBoundary>
         </React.Fragment>
     );
 }
