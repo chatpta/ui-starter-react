c14ca1df899df36ecde3ac683c3d367a05ba09e2
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-11-20 22:18:20 -0500

    working

--- src/test/component/App.test.js
+++ src/test/component/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from '../../App/App';
+import { MemoryRouter } from "react-router-dom";
 
 test('renders home page link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/home page/i);
+  render(<App /> );
+  const linkElement = screen.getByText(/Loading.../i);
   expect(linkElement).toBeInTheDocument();
 });
