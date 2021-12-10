e570535188df54f67e9d1946b8da9aff3ede827e
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-12-10 14:08:58 -0500

    refactored

--- src/pages/authPages/register/CreateAccountElementLib.js
+++ src/pages/authPages/register/CreateAccountElementLib.js
@@ -1,27 +1,37 @@
 function handlers( user, userMutate, userFetch, userReset ) {
 
     const firstNameChange = ( event ) => {
-        userMutate( { firstName: event.target.value } )
+
+        userMutate( { firstName: event.target.value } );
+
     }
 
     const emailChange = ( event ) => {
-        userMutate( { email: event.target.value } )
+
+        userMutate( { email: event.target.value } );
+
     }
 
     const passwordChange = ( event ) => {
-        userMutate( { password: event.target.value } )
+
+        userMutate( { password: event.target.value } );
+
     }
 
     const clickCreateUser = e => {
+
         e.stopPropagation();
         e.preventDefault();
+
         if ( user?.firstName && user?.email && user?.password ) {
             userFetch( postReq( JSON.stringify( { user } ) ) );
             userReset( {} );
         }
+
     }
 
     function postReq( body ) {
+
         return new Request( "http://localhost:3001/users",
             {
                 method: 'POST',
