d2ede6f4dfbbfa2cddb4ef579b87cc00ff45622a
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-12-10 19:07:30 -0500

    login element added

--- src/pages/authPages/login/LoginElement.js
+++ src/pages/authPages/login/LoginElement.js
@@ -1,104 +1,111 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { Hidden } from "@mui/material";
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
 import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-import useLoginElementStyle from "./LoginElementStyle";
-
-
-function LoginElement( props ) {
-    const { userFetch } = props;
-
-    const classes = useLoginElementStyle();
-
-    const [ userLogin, setUserLogin ] = React.useState( { username: '', password: '' } );
-
-    const postReq = body => new Request( "http://localhost:3001/login",
-        {
-            method: 'POST',
-            headers: {
-                "Accept": "application/json",
-                "Content-type": "application/json",
-                'Access-Control-Allow-Origin': window.location.origin
-            },
-            body: body
-        } );
-
-
-    const handleClickLogin = e => {
-        e.stopPropagation();
-        e.preventDefault();
-        if ( userLogin?.username && userLogin?.password ) {
-            userFetch( postReq( JSON.stringify( userLogin ) ) );
-        }
-    };
-
-
+function Copyright(props) {
     return (
-        <div className={ classes.loginRoot }>
-            <div className={ classes.innerContainer }>
-                <div className={ classes.contactFormContainer }>
-                    <h2 className={ classes.title }>Login</h2>
-                    <h4 className={ classes.title }>
-                        <Hidden xsDown> Do not have account ? </Hidden>
-                        <Link to={ "/users/register" } style={ { textDecoration: 'none' } }>
-                            <span className={ classes.createAccount }>Register !</span>
-                        </Link>
-                    </h4>
-                    <TextField
-                        id="username"
-                        variant="outlined"
-                        placeholder={ "Username" }
-                        color="primary"
-                        size={ "small" }
-                        fullWidth
-                        className={ classes.textField }
-                        value={ userLogin.username }
-                        onChange={ event => setUserLogin( { ...userLogin, username: event.target.value } ) }
-                    />
-                    <TextField
-                        id="password"
-                        variant="outlined"
-                        placeholder={ "Password" }
-                        color="primary"
-                        size={ "small" }
-                        type="password"
-                        fullWidth
-                        className={ classes.textField }
-                        value={ userLogin.password }
-                        onChange={ event => setUserLogin( { ...userLogin, password: event.target.value } ) }
-                    />
-                    <div className={ classes.rememberLine }>
-                        <FormControlLabel
-                            control={ <Checkbox
-                                checked={ true }
-                                onChange={ null }
-                                name="remember"
-                                color={ "primary" }
-                            /> }
-                            label="Remember me"
-                        />
-                        <Link to={ "/recover" } style={ { textDecoration: 'none' } }>
-                            <p className={ classes.forgotPassword }>Forgot password?</p>
-                        </Link>
-                    </div>
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        className={ classes.sendButton }
-                        size={ "large" }
-                        fullWidth
-                        onClick={ handleClickLogin }
-                    >
-                        Send
-                    </Button>
-                </div>
-            </div>
-        </div>
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://mui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
     );
 }
 
-export default LoginElement;
+const theme = createTheme();
+
+export default function SignIn() {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // eslint-disable-next-line no-console
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign in
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                        <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign In
+                        </Button>
+                        <Grid container>
+                            <Grid item xs>
+                                <Link href="#" variant="body2">
+                                    Forgot password?
+                                </Link>
+                            </Grid>
+                            <Grid item>
+                                <Link href="#" variant="body2">
+                                    {"Don't have an account? Sign Up"}
+                                </Link>
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </ThemeProvider>
+    );
+}
---
+++ src/pages/authPages/login/LoginElementStyleCopy.js
@@ -0,0 +1,72 @@
+import { makeStyles } from "@mui/styles";
+
+const useLoginElementStyle = makeStyles( theme => ( {
+    loginRoot: () => ( {
+        minHeight: "76vh",
+        display: "flex",
+        alignItems: "center",
+        '@media (min-width: 700px)': {
+            padding: theme.spacing( 16 ),
+        },
+        '@media (min-width: 1024px)': {
+            padding: theme.spacing( 20 ),
+        },
+        '@media (min-width: 1800px)': {
+            padding: theme.spacing( 30 ),
+        },
+    } ),
+    innerContainer: () => ( {
+        maxWidth: theme.dimensions.mainSectionWidth,
+        padding: theme.spacing( 2 ),
+        margin: "auto",
+        '@media (min-width: 660px)': {
+            display: "flex",
+            flexFlow: "row",
+            paddingBottom: theme.spacing( 2 ),
+        }
+    } ),
+    contactFormContainer: () => ( {
+        margin: "auto",
+        backgroundColor: "white",
+        padding: theme.spacing( 4 ),
+        borderRadius: "6px",
+        minHeight: 200,
+        '&>*': {
+            margin: 0,
+        },
+        '@media (min-width: 760px)': {
+            order: 2,
+            padding: theme.spacing( 2 ),
+            paddingTop: theme.spacing( 4 ),
+        }
+    } ),
+    title: () => ( {
+        textAlign: "center",
+    } ),
+    createAccount: () => ( {
+        color: theme.palette.secondary.main,
+        cursor: "pointer",
+    } ),
+    textField: () => ( {
+        marginTop: theme.spacing( 2 ),
+    } ),
+    rememberLine: () => ( {
+        display: "flex",
+        flexFlow: "row",
+        justifyContent: "space-between",
+    } ),
+    forgotPassword: () => ( {
+        color: theme.palette.primary.main,
+        cursor: "pointer",
+    } ),
+    sendButton: () => ( {
+        // color: "white",
+        textTransform: "none",
+        fontWeight: 800,
+        '&>span': {
+            paddingTop: theme.spacing( 0.4 ),
+        }
+    } ),
+} ), { index: 1 } );
+
+export default useLoginElementStyle;
