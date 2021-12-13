3668bb29c8bfd2ccff58dbaf9f7b0ed921b8ada1
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-12-13 16:46:51 -0500

    store connect function created

---
+++ src/pages/authPages/lib/storeConnect.js
@@ -0,0 +1,30 @@
+import { connect } from "react-redux";
+import reduxStoreUtility from "@chatpta/redux-store-utility";
+
+const prSct = reduxStoreUtility.createActions( "User" );
+const userMutate = dispatch => keyValue => dispatch( prSct.userMutate( keyValue ) );
+const userReset = dispatch => resetValue => dispatch( prSct.userReset( resetValue ) );
+const prsAct = reduxStoreUtility.createAsyncActions( "User" );
+const userFetch = dispatch => request => dispatch( prsAct.userFetch( request ) );
+
+
+function StoreConnect( Element ) {
+
+    const mapStateToProps = state => {
+        return {
+            user: state.user,
+        };
+    };
+
+    const mapDispatchToProps = dispatch => {
+        return {
+            userFetch: userFetch( dispatch ),
+            userMutate: userMutate( dispatch ),
+            userReset: userReset( dispatch )
+        };
+    };
+
+    return connect( mapStateToProps, mapDispatchToProps )( Element );
+}
+
+export default StoreConnect;
