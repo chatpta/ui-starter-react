b9da9fd733b00c65450137d419569f16e243468c
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2021-12-14 13:21:03 -0500

    connected to store

--- src/pages/authPages/register/CreateAccountPage.js
+++ src/pages/authPages/register/CreateAccountPage.js
@@ -1,28 +1,4 @@
-import { connect } from "react-redux";
-import reduxStoreUtility from "@chatpta/redux-store-utility";
 import CreateAccountElement from "./CreateAccountElement";
+import storeConnect from "../../../store/storeConnectUserEdit";
 
-const prSct = reduxStoreUtility.createActions( "User" );
-const userMutate = dispatch => keyValue => dispatch( prSct.userMutate( keyValue ) );
-const userReset = dispatch => resetValue => dispatch( prSct.userReset( resetValue ) );
-
-const prsAct = reduxStoreUtility.createAsyncActions( "User" );
-const userFetch = dispatch => request => dispatch( prsAct.userFetch( request ) );
-
-const mapStateToProps = state => {
-    return {
-        user: state.user,
-    };
-};
-
-const mapDispatchToProps = dispatch => {
-    return {
-        userFetch: userFetch( dispatch ),
-        userMutate: userMutate( dispatch ),
-        userReset: userReset( dispatch )
-    };
-};
-
-const CreateAccountPage = connect( mapStateToProps, mapDispatchToProps )( CreateAccountElement );
-
-export default CreateAccountPage;
+export default storeConnect( CreateAccountElement );
