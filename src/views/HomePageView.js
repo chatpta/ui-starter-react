b3042644d6ea37178eea8912b141976c692875cc
PK Singh <pksin@ulaval.ca>
2021-12-14 09:54:16 -0500

    favicon changed

--- src/views/HomePageView.js
+++ src/views/HomePageView.js
@@ -1,40 +1,10 @@
-import { Link, NavLink } from "react-router-dom";
 import React from "react";
-import LinkCH from "../components/LinkCH/LinkCH";
-import ErrorBoundary from "../ErrorBoundry/ErrorBoundary";
 import LandPage from "../pages/landingPage/land/LandPage";
-import Header from "../sections/header/Header";
 
 export default function HomePageView() {
     return (
-        <React.Fragment>
-            <Header/>
-            <h1>Home Page</h1>
+        <div>
             <LandPage/>
-            <ErrorBoundary>
-                <nav
-                    style={ {
-                        borderBottom: "solid 1px",
-                        paddingBottom: "1rem"
-                    } }
-                >
-                    <Link to="/users">Users</Link> | { " " }
-                    <Link to="/items">Items</Link> | { " " }
-                    <LinkCH page={ "/users" }>Go to Users page</LinkCH> | { " " }
-                    <NavLink
-                        style={ ( { isActive } ) => {
-                            return {
-                                display: "block",
-                                margin: "1rem 0",
-                                color: isActive ? "green" : "blue"
-                            };
-                        } }
-                        to={ '/users' }
-                    >
-                        Nav Link
-                    </NavLink>
-                </nav>
-            </ErrorBoundary>
-        </React.Fragment>
+        </div>
     );
 }
