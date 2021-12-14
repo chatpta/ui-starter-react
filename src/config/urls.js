83dc23f7a1ad6808d1baedcf58bfd414cd7513b6
PK Singh <pksin@ulaval.ca>
2021-12-14 07:35:07 -0500

    /api is prepended to all non 'development' urls

--- src/config/urls.js
+++ src/config/urls.js
@@ -20,28 +20,31 @@ module.exports = {
     getHost: function () {
         return ( process?.env?.NODE_ENV === 'development' ? this.localRoot : this.root );
     },
+    getHostApi: function () {
+        return ( process?.env?.NODE_ENV === 'development' ? this.getHost() : this.getHost() + "/api" );
+    },
     usersLoginPath: function () {
         return this.paths.users.root + this.paths.users.login;
     },
     usersLoginURL: function () {
-        return this.getHost() + this.usersLoginPath();
+        return this.getHostApi() + this.usersLoginPath();
     },
     usersRegisterPath: function () {
         return this.paths.users.root + this.paths.users.register;
     },
     usersRegisterURL: function () {
-        return this.getHost() + this.usersRegisterPath();
+        return this.getHostApi() + this.usersRegisterPath();
     },
     usersPasswordRecoverPath: function () {
         return this.paths.users.root + this.paths.users.recover;
     },
     usersPasswordRecoverURL: function () {
-        return this.getHost() + this.usersPasswordRecoverPath();
+        return this.getHostApi() + this.usersPasswordRecoverPath();
     },
     usersChangePasswordPath: function () {
         return this.paths.users.root + this.paths.users.changePassword;
     },
     usersChangePasswordURL: function () {
-        return this.getHost() + this.usersChangePasswordPath();
+        return this.getHostApi() + this.usersChangePasswordPath();
     },
 }
