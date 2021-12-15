bdfba624882ac19d4a490d48fa229c60a21ea6c1
PK Singh <pksin@ulaval.ca>
2021-12-15 07:55:53 -0500

    Global style added

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,12 @@
 import DefaultRouter from "../router/DefaultRouter";
 import "./app.css";
+import { GlobalStyles } from "@mui/material";
 
 function App() {
     return (
         <div className="App">
             <div className={ "overlay" }>
+                <GlobalStyles styles={ { body: { backgroundColor: "rgb(26, 35, 39)" } } }/>
                 <DefaultRouter/>
             </div>
         </div>
