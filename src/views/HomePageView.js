b19df535a45b622950ba290dee6bc035e6bc71b6
PK Singh <pksin@ulaval.ca>
2021-12-16 06:36:42 -0500

    Footer removed

--- src/views/HomePageView.js
+++ src/views/HomePageView.js
@@ -1,14 +1,12 @@
 import React from "react";
 import LandingPage from "../pages/landingPage/land/LandingPage";
 import AppBarChatpta from "../sections/header/AppBar/AppBarChatpta";
-import Footer from "../sections/footer/FooterComponent";
 
 export default function HomePageView() {
     return (
         <div>
             <AppBarChatpta transparent/>
             <LandingPage/>
-            <Footer/>
         </div>
     );
 }
