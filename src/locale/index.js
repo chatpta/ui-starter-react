d7d6b7db89ab4c84823fc832cfbdf1e6123f9439
Pankaj Priscilla <pankajpriscilla@gmail.com>
2023-02-12 12:51:46 -0500

    t function created

---
+++ src/locale/index.js
@@ -0,0 +1,17 @@
+import headerEn from "./languages/en/header.json";
+import headerFr from "./languages/fr/header.json";
+import { selectDisplayLanguage } from "./i18n/i18n";
+
+export function t() {
+    const languages = {
+        en: {
+            header: headerEn
+        },
+        fr: {
+            header: headerFr
+        }
+    }
+
+    return languages[ selectDisplayLanguage( languages ) ];
+
+}
